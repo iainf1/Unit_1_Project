aaef82e687280f3cd0d65ee6fb739914aaf61b49
Iain Fulnecky <iainf@nycstudents.net>
2025-09-22 08:41:19 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your Bill($):  ");
+        double bill = (s.nextDouble());
+        System.out.println("Tip Percent: ");
+        int tipPercent = (s.nextInt());
+        System.out.println("Number of People: ");
+        int ppl = (s.nextInt());
+        double totalTip = (bill*());
+        System.out.print(totalTip);
+        
+    }
+}
