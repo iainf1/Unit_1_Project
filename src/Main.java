7bc44f758c33e9751b12da04b283bd325f7d7eb3
IainFulnecky <iainf1@email.com>
2025-09-25 08:42:05 -0400

    added calculation logic

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter your Bill($):  ");
-        double bill = (s.nextDouble());
-        System.out.println("Tip Percent: ");
+        System.out.println("----------------------------------------------------------------");
+        System.out.print("Enter your Bill($): ");
+        int billInt = (int) (s.nextDouble()*10.0);
+        System.out.print("Tip Percent: ");
         int tipPercent = (s.nextInt());
-        System.out.println("Number of People: ");
+        System.out.print("Number of People: ");
         int ppl = (s.nextInt());
-        double totalTip = (bill*());
-        System.out.print(totalTip);
-        
+        double tip = (billInt*(tipPercent/1000.0));
+        double total = ((double) billInt/10+tip);
+        double tipPerPerson = ((int) ((tip/ppl)*100)/100.0);
+        double totalPerPerson = ((int) ((total/ppl)*100)/100.0);
+        System.out.println();
+        System.out.println("--------------------------------");
+        System.out.println("Total Tip Amount: $" + tip + "        |");
+        System.out.println("--------------------------------");
+        System.out.println("Total bill cost: $" + total + "        |");
+        System.out.println("--------------------------------");
+        System.out.println("Tip per person: $" + tipPerPerson + "          |");
+        System.out.println("--------------------------------");
+        System.out.println("Total bill per person: $" + totalPerPerson + "   |");
+        System.out.println("--------------------------------");
+
+
     }
 }
